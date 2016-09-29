e6cca5d59aafd097e46b14aff7760353475cfd87
raymond <showgp@163.com>
2016-09-29 11:03:04 +0800

    下一步进行博客页面的工作, 主要是列表和详情也是...另外准备开始下拉刷新逻辑, 以及不等高cell.

--- iosAPP_Swift/Application/APIConstants.swift
+++ iosAPP_Swift/Application/APIConstants.swift
@@ -9,8 +9,15 @@
 import Foundation
 
 let BASE_REQUEST_ADDRESS = "http://www.oschina.net/action/api"
+
+//新闻列表和详情
 let NEWS_LIST = BASE_REQUEST_ADDRESS + "/news_list"
 let NEWS_DETAIL = BASE_REQUEST_ADDRESS + "/news_detail"
 let NOTIFICATION_NEWS_ARRIVE = NSNotification.Name(rawValue: "END_RETRIVE_NEWS_LIST")
 let NOTIFICATION_NEWS_DETAIL = NSNotification.Name(rawValue: "END_RETRIVE_NEWS_DETAIL")
+
+//博客列表和详情
+let BLOG_LIST = BASE_REQUEST_ADDRESS + "/blog_list"
+
+
 let NOTIFICATION_USERINFO_NEWS = "NewsList"
--- iosAPP_Swift/Controllers/NewsSubVC/SubBlogTableViewController.swift
+++ iosAPP_Swift/Controllers/NewsSubVC/SubBlogTableViewController.swift
@@ -9,20 +9,14 @@
 import UIKit
 
 class SubBlogTableViewController: UITableViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.brown
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
